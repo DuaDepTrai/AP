c810761aa8f5a3e875974f68e1ce36566f0f36a0
DuaDepTrai <hieun.mgc@gmail.com>
2025-02-13 23:54:31 +0700

    update

---
+++ 250212.Homework1/Product/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace ProductManagement
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int[] Rate { get; set; }
+        private int rateCount;
+
+        public Product(string name, string description, double price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Rate = new int[100];
+            rateCount = 0;
+        }
+
+        public void addRating(int rating) 
+        {
+            if (rating < 1 || rating > 5) return;
+            if (rateCount < Rate.Length) Rate[rateCount++] = rating;
+        }
+
+        public double GetAverageRating()
+        {
+            if (rateCount == 0) return 0;
+            int sum = 0;
+            for (int i = 0; i < rateCount; i++) sum += Rate[i];
+            return (double)sum / rateCount;
+        }
+
+        public void ViewInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Price: {Price}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Average Rating: {GetAverageRating(): 0.00}");
+        }
+    }
+}
---
+++ 250212.Homework2/Savings/Savings.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+using BankAccountManagement;
+
+namespace SavingsLibrary;
+public class Savings : BankAccount
+{
+    public float InterestRate { get; set; }
+
+    public Savings(string accountName, float balance, float interestRate)
+        : base(accountName, balance)
+    {
+        InterestRate = interestRate;
+    }
+
+    public void CalculateInterest(int months)
+    {
+        float interestBalance = Balance;
+        for (int i = 0; i < months; i++)
+        {
+            interestBalance = (interestBalance * InterestRate) / 100 + interestBalance;
+        }
+        Console.WriteLine($"Account: {Name} | After {months} months, new balance with interest: {interestBalance}");
+    }
+}
