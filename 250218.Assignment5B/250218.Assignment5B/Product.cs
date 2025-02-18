d80bead81957e679516b7aab231b3379f97511cf
DuaDepTrai <hieun.mgc@gmail.com>
2025-02-19 01:45:37 +0700

    update

---
+++ 250218.Assignment5B/250218.Assignment5B/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement
+{
+    public class Products
+    {
+        private static int Count = 0;
+        public string ID { get; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public Products()
+        {
+            Count++;
+            ID = "Hang" + Count.ToString();
+            Name = "No name";
+            Quantity = 0;
+        }
+
+        public void ShowDetail()
+        {
+            Console.WriteLine("{0,-10} {1,-20} {2,-10}", ID, Name, Quantity);
+        }
+
+        public void SetDetail(string name, int quantity)
+        {
+            this.Name = name;
+            this.Quantity = quantity;
+        }
+
+        public void AddQuantity(int addQuantity)
+        {
+            this.Quantity += addQuantity;
+        }
+    }
+}
---
+++ 250218.Assignment5B/250218.Assignment5B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProductManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Product Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Quantity: ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            Products objProduct = new Products();
+            objProduct.SetDetail(name, quantity);
+
+            Console.WriteLine("\nProduct Information:");
+            Console.WriteLine("ID         Name                 Quantity");
+            objProduct.ShowDetail();
+
+            Console.Write("\nEnter More Quantity to Add: ");
+            int moreQuantity = int.Parse(Console.ReadLine());
+
+            objProduct.AddQuantity(moreQuantity);
+
+            Console.WriteLine("\nProduct Information after Update:");
+            Console.WriteLine("ID         Name                 Quantity");
+            objProduct.ShowDetail();
+        }
+    }
+}
