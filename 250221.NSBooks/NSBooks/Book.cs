7b65ee808bf5efd58a1dc6804bde3e98063807ed
DuaDepTrai <hieun.mgc@gmail.com>
2025-02-24 23:59:12 +0700

    update

---
+++ 250221.NSBooks/NSBooks/Book.cs
@@ -0,0 +1,64 @@
+﻿namespace NSBooks
+{
+    public class Book:IBook
+    {
+        private static int Count = 0;
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Author { get; set; }
+        public string Language { get; set; }
+        
+        public float _AveragePrice;
+        public float AveragePrice { get { return _AveragePrice; } }
+
+
+        public Book (string name, DateTime publishDate, string author, string language)
+        {
+            Count++;
+            ID = Count;
+            Name = name;
+            PublishDate = publishDate;
+            Author = author;
+            Language = language;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine ($"Name: {Name}, Publish Date: {PublishDate}, Author: {Author}, Language: {Language}, Average Price: {AveragePrice}");
+        }
+
+
+        private static int?[] PriceList = new int?[5];
+
+        float IBook.this[int id] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int? this[int index]
+        {
+            get 
+            { 
+                if ( index >=0 && index < PriceList.Length )
+                {
+                    return PriceList[index];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException ();
+                }
+            }
+            set { PriceList[index] = value; }
+        }
+
+        public void Calculate()
+        {
+            //if () throw new ArgumentNullException("Price list not has any element");
+            float? sum = 0;
+            int count = 0;
+            foreach (float? price in PriceList) 
+            {
+                count++;
+                sum += price;
+            }
+            _AveragePrice = (float)sum / count;
+        }
+    }
+}
---
+++ 250221.NSBooks/NSBooks/BookManagement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using NSBooks;
+
+namespace NSBookTest
+{
+    internal class BookManagement
+    {
+        private ArrayList bookList = new ArrayList();
+
+        public void AddBook()
+        {
+            Console.WriteLine("Enter Book's Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Publish Date: ");
+            DateTime pbDate;
+            while (!DateTime.TryParse(Console.ReadLine(), out pbDate))
+            {
+                Console.WriteLine("Invalid format, please try again");
+            }
+
+            Console.WriteLine("Enter Author: ");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Enter Language: ");
+            string language = Console.ReadLine();
+
+            Book book = new Book(name, pbDate, author, language);
+
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine($"Enter price in store {i}: ");
+                                
+            }
+
+            bookList.Add( book );
+        }
+
+        public 
+    }
+}
